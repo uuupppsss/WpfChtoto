df0bb6458b693134ec375f7bc72648f21b742f64
uuupppsss <ulya.ch.06@mail.ru>
2024-10-11 13:40:47 +1000

    Добавьте файлы проекта.

---
+++ WpfChtoto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfChtoto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
